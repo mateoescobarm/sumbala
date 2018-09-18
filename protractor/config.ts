5a707fa9edf80302a9f3bdafe0aaad7428227df4
Rene Marin <rene@su.local>
2018-09-18 17:52:18 -0500

    Void Bot

---
+++ protractor/config.ts
@@ -0,0 +1,25 @@
+import { browser, Config } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+export const config: Config = {
+  framework: 'jasmine',
+  SELENIUM_PROMISE_MANAGER: false,
+  specs: ['../test/*.spec.js'],
+  noGlobals: true,
+  getPageTimeout: 50000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 200000
+  },
+  onPrepare: () => {
+    browser.manage().timeouts().implicitlyWait(0);
+    browser.ignoreSynchronization = true;
+    reporter();
+  },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['disable-infobars=true --window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  }
+};
---
+++ protractor/headless.config.ts
@@ -0,0 +1,24 @@
+import { browser, Config } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+export const config: Config = {
+  framework: 'jasmine',
+  SELENIUM_PROMISE_MANAGER: false,
+  specs: ['../test/*.spec.js'],
+  noGlobals: true,
+  getPageTimeout: 30000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
+  onPrepare: () => {
+    browser.manage().timeouts().implicitlyWait(0);
+    browser.ignoreSynchronization = true;
+    reporter();
+  },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu', '--window-size=800,600']
+    }
+  }
+};
